963d89779e59f7471790e76ab1892d8f165f4733
Billy Mihalarias <billy.mihalarias@aon.com>
2022-09-26 13:18:19 +1000

    Added Lesson 1 code

---
+++ Lesson1/loops.py
@@ -0,0 +1,17 @@
+#number = 0
+
+a_list = ["hi","i","am","dave"]
+
+#print(len(a_list))
+#print(a_list[0])
+
+#c style loop
+"""
+while number < len(a_list):
+    print(number)
+    print(a_list[number])
+    number = number + 1
+"""
+for word in a_list:
+    print(word)
+
---
+++ Lesson1/my_frist_script.py
@@ -0,0 +1,32 @@
+print("hello world")
+
+boolean_variable_one = False
+boolean_variable_two = False
+#print("Variable 1: " + str(boolean_variable_one))
+#print("Variable 2: " + str(boolean_variable_two))
+
+
+if boolean_variable_one:
+    pass
+    #print("variable 1 was true")
+    #print("hi")
+elif boolean_variable_two:
+    pass
+    #print("variable 2 is true")
+else:
+    pass
+    #print("both are false")
+
+
+its_monday = True
+im_hungry = False
+
+print(not its_monday)
+
+
+if not its_monday or im_hungry:
+    print("OR worked")
+
+
+print("goodbye world")
+
